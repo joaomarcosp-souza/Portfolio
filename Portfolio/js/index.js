1cb836a0ddb7f3aaaada158a9d1a7b15e5772d0a
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-13 12:52:41 -0300

    Funcionalidade de armazenar o Tema do Portfolio

--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+    verificarTema();
 });
 
+function verificarTema(){
+    if(localStorage.getItem('tema')){
+        document.body.setAttribute('data-theme', localStorage.getItem('tema'));
+    }
+}
 
 function alterarTema(){
     const tema = document.body.getAttribute('data-theme');
     const temaNovo = tema == 'dark' ? 'light' : 'dark';
     document.body.setAttribute('data-theme', temaNovo);
+    localStorage.setItem('tema', temaNovo);
 }
