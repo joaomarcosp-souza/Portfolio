7c4b845463eed495b2bb0385a34732506aeb8ea6
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2024-07-15 15:38:35 -0300

    pequena correção, API do GitHub.

--- Portfolio/js/gitAPI.js
+++ Portfolio/js/gitAPI.js
@@ -1,37 +1,39 @@
-const repositories = document.querySelector('.principal');
+const repositories = document.querySelector(".principal");
 
 function getApi() {
-    fetch('https://api.github.com/users/joaomarcosp-souza/repos')
-        .then(async response => {
-            if (!response.ok) {
-                throw new Error('Erro na requisição');
-            }
-            let data = await response.json();
+  const headers = {
+    Authorization: "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",
+  };
 
-            data.map(item => {
-                let project = document.createElement('div');
-
-                project.innerHTML = `
-                   <div class="card">
-                    <div class="content">
-                        <div class="info">
-                            <h5>${item.name}</h5>
-                            <p>${item.description || "sem descrição"}</p>
+  fetch("https://api.github.com/users/joaomarcosp-souza/repos", { headers })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error("Erro na requisição");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((item) => {
+        let project = document.createElement("div");
+        project.innerHTML = `
+            <div class="card">
+                <div class="content">
+                    <div class="info">
+                        <h5>${item.name}</h5>
+                        <p>${item.description || "sem descrição"}</p>
+                    </div>
+                    <div class="tags-e-banner">
+                        <div class="tags">
+                            <div class="tecnologias-tag">${item.language}</div>
                         </div>
-                        <div class="tags-e-banner">
-                            <div class="tags">
-                                <div class="tecnologias-tag">${item.language}</div>
-                            </div>
-                        </div>
-                        <div class="link">
-                            <a href="${item.html_url}" target="_blank">Ver projeto</a>
                     </div>
                 </div>
-                `
-                repositories.appendChild(project);
-        })
+            </div>
+        `;
+        repositories.appendChild(project);
+      });
     })
+    .catch((error) => console.error("Falha na requisição:", error));
 }
-
 
 getApi();
--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    verificarTema();
+document.addEventListener("DOMContentLoaded", () => {
+  verificarTema();
 });
 
-function verificarTema(){
-    if(localStorage.getItem('tema')){
-        document.body.setAttribute('data-theme', localStorage.getItem('tema'));
-    }
+function verificarTema() {
+  if (localStorage.getItem("tema")) {
+    document.body.setAttribute("data-theme", localStorage.getItem("tema"));
+  }
 }
 
-function alterarTema(){
-    const tema = document.body.getAttribute('data-theme');
-    const temaNovo = tema == 'dark' ? 'light' : 'dark';
-    document.body.setAttribute('data-theme', temaNovo);
-    localStorage.setItem('tema', temaNovo);
+function alterarTema() {
+  const tema = document.body.getAttribute("data-theme");
+  const temaNovo = tema == "dark" ? "light" : "dark";
+  document.body.setAttribute("data-theme", temaNovo);
+  localStorage.setItem("tema", temaNovo);
 }
